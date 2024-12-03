1f4196021ff323d42527f12a1dc157d69a982b4b
Vinayak Aggarwal <vinayakaggarwal05@gmail.com>
2024-12-03 18:07:09 +0530

    first commit

---
+++ src/main/java/com/lazyengineer/api/LazyEngineerApiApplication.java
@@ -0,0 +1,13 @@
+package com.lazyengineer.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LazyEngineerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LazyEngineerApiApplication.class, args);
+	}
+
+}
